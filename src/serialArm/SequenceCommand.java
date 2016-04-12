5f8b5603833df333e8495ed49d81884e8fee7390
randomtestfive <randomtestfive@gmail.com>
2016-04-12 19:20:01 -0400

    Fix alignment issues

--- src/serialArm/SequenceCommand.java
+++ src/serialArm/SequenceCommand.java
@@ -1,9 +1,13 @@
 package serialArm;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -24,24 +28,38 @@ public class SequenceCommand extends JPanel implements ActionListener
 	public SequenceCommand(String name, String direction, boolean very) 
 	{
 		super();
+		setAlignmentX(JPanel.LEFT_ALIGNMENT);
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		//setPreferredSize(new Dimension(300, 40));
 		n = name;
 		d = direction;
 		v = very;
+		x = new JButton("X");
+		x.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+		add(x);
+		add(Box.createRigidArea(new Dimension(2,0)));
+		up = new JButton("▲");
+		up.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+		add(up);
+		add(Box.createRigidArea(new Dimension(2,0)));
+		down = new JButton("▼");
+		down.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+		add(down);
+		add(Box.createRigidArea(new Dimension(5,0)));
 		label = new JLabel("Motor: " + name);
 		label.setForeground(Color.black);
+		label.setAlignmentX(JPanel.LEFT_ALIGNMENT);
 		add(label);
+		add(Box.createRigidArea(new Dimension(5,0)));
 		directionLabel = new JLabel("Direction: " + direction);
 		directionLabel.setForeground(Color.blue);
+		directionLabel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
 		add(directionLabel);
+		add(Box.createRigidArea(new Dimension(5,0)));
 		veryLabel = new JLabel("Very: " + very);
 		veryLabel.setForeground(Color.red);
+		veryLabel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
 		add(veryLabel);
-		up = new JButton("▲");
-		add(up);
-		down = new JButton("▼");
-		add(down);
-		x = new JButton("X");
-		add(x);
 		x.addActionListener(this);
 	}
 
